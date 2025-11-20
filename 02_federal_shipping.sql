5ef4f1d35f9b063a5c945ac1aae3cd8bfff8eabb
origin <acmuhammadmustafa@gmail.com>
2025-11-19 20:57:47 -0500

    question 2 complete

---
+++ 02_federal_shipping.sql
@@ -0,0 +1,4 @@
+use northwind;
+select OrderID, CustomerID, EmployeeID, OrderDate, RequiredDate, ShippedDate, ShipVia, Freight, ShipName, ShipAddress, ShipCity, ShipRegion, ShipPostalCode, ShipCountry
+from orders
+where ShipVia = 3
